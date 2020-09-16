af9cfa07b79d9de98547a5e58d8de962799aabb6
Jojen <zhoujianxiao96@gmail.com>
2020-09-16 15:46:47 +0800

    Switch opencv to imageio in user_preprocess

--- user_preprocess.py
+++ user_preprocess.py
@@ -7,36 +7,28 @@ import os
 import multiprocessing
 from functools import partial
 import time
+import imageio
 
-def mul_preprocess(h_dir, hour, fourcc, video_file):
+
+def mul_preprocess(h_dir, hour, video_file):
     video_name = video_file.split('.')[0]
     video_path = os.path.join(h_dir, video_file)
-    cap = cv2.VideoCapture(video_path)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     output_path = os.path.join(h_dir, "{}_{}.mp4".format(hour, video_name))
-    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
-    while True:
-        # ret返回布尔量
-        ret, frame = cap.read()
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            eq = cv2.equalizeHist(gray)
-            eq_1 = cv2.cvtColor(eq, cv2.COLOR_GRAY2BGR)
-            out.write(eq_1)
-        else:
-            break
-    out.release()
-    cap.release()
+    reader = imageio.get_reader(video_path)
+    writer = imageio.get_writer(output_path, fps=reader.get_meta_data()['fps'])
+    for _, im in enumerate(reader):
+        im = cv2.resize(im, (464, 256))
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        eq = cv2.equalizeHist(gray)
+        eq_1 = cv2.cvtColor(eq, cv2.COLOR_GRAY2BGR)
+        writer.append_data(eq_1)
+    writer.close()
     os.remove(video_path)
     os.rename(output_path, video_path)
 
 
 def video_preprocess(day_dir, mul_num=1):
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
     hours = os.listdir(day_dir)
     try:
         multiprocessing.set_start_method('spawn')
@@ -47,10 +39,10 @@ def video_preprocess(day_dir, mul_num=1):
         minutes = os.listdir(h_dir)
         if mul_num == 1:
             for video_file in minutes:
-                mul_preprocess(h_dir, hour, fourcc, video_file)
+                mul_preprocess(h_dir, hour, video_file)
         else:
             pool = multiprocessing.Pool(mul_num)
-            func = partial(mul_preprocess, h_dir, hour, fourcc)
+            func = partial(mul_preprocess, h_dir, hour)
             pool.map(func, minutes)
             pool.close()
             pool.join()
@@ -58,6 +50,6 @@ def video_preprocess(day_dir, mul_num=1):
 
 if __name__ == '__main__':
     start_time = time.time()
-    video_preprocess("/Users/jojen/Workspace/cityU/data/test/2020-05-10")
+    video_preprocess("/Users/jojen/Workspace/cityU/data/test/2020-05-11")
     end_time = time.time()
     print("Cost {} seconds.".format(end_time - start_time))
